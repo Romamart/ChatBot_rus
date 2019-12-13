6e28821060ce43c4ec2b1c7f5f2623cab31d9134
Loulaan <ivan.kononihin@gmail.com>
2019-12-13 21:17:08 +0300

    rejection of print

--- server.py
+++ server.py
@@ -6,15 +6,12 @@ async def handle_echo(reader, writer):
     data = await reader.read(100)
     message = data.decode()
     addr = writer.get_extra_info('peername')
-    print("Received %r from %r" % (message, addr))
     
     botAnswer = evaluateInput(message)
 
-    print("Send: %r" % botAnswer)
     writer.write(botAnswer.encode())
     await writer.drain()
 
-    print("Close the client socket")
     writer.close()
 
 loop = asyncio.get_event_loop()
@@ -22,7 +19,6 @@ coro = asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)
 server = loop.run_until_complete(coro)
 
 # Serve requests until Ctrl+C is pressed
-print('Serving on {}'.format(server.sockets[0].getsockname()))
 try:
     loop.run_forever()
 except KeyboardInterrupt:
