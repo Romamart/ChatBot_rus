347618ae4297dc619948f4135681a8976cff80c7
Romamart <martinyuk.r@yandex.ru>
2019-12-14 05:40:17 +0300

    Fix Fitches

--- Train.py
+++ Train.py
@@ -220,8 +220,8 @@ if __name__ == '__main__':
 
     clip = 50.0
     teacher_forcing_ratio = 1.0
-    learning_rate = 0.0001
-    decoder_learning_ratio = 5.0
+    learning_rate = 0.0005
+    decoder_learning_ratio = 1.0
     n_iteration = 4000
     print_every = 10
     save_every = 500
